e3e28fe8fd8fc028b04a0ae1d76edb5eba6454e0
Sylvester Freeman <sylfree1029@gmail.com>
2022-08-31 14:49:26 -0400

    Update

--- Form/js/check.js
+++
@@ -1,6 +0,0 @@
-function validate(){
-    var phone = document.getElementById("phone");
-    if(! /^[7-9][0-9]{9}$/.check(phone)){
-        document.getElementById("phone").setAttribute('value','My default value');
-    }
-}
